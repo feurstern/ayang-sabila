a66c29b24a91fb22495a1dbe2259699e2cdc9791
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2022-07-08 19:33:44 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Ayang Sabila",
+    introTitle: 'Haiiii, malam Sabila. Kangennn sama kamuuu ',
+    introDesc: `Tolong kamu coba explore webnya ya. Mungkin aja ada sesuatu yang menarik wkkwwk. Btw, coba turn on volume-nya atau enggak pake earphone ya.`,
+    btnIntro: 'Next',
+    title: 'Kamu sayang sama aku enggak sih?',
+    desc: 'Jawab aja yang jujur gpp kok. Oh iya btw, button "enggak" lagi error. Jadinya cuma bisa pake button satu lagi wkkwkwkw',
+    btnYes: 'Iyaaaa',
+    btnNo: 'Enggakkkkk',
+    question: 'Ihhhh makasih jawabannya ayang Sabila. Apa ada hal lain yang mau disampaikan?',
+    btnReply: 'Next',
+    reply: '',
+    mess: 'Makasihhh ayang udah bukan web-nya',
+    messDesc: 'Aku lakuin begini emang beneran serius kok sama kamu, Sabila . Nanti aku akan tunjukin hal - hal yang lain buat buktiin kalo aku beneran serius sama kamu. Btw, happy weekend .Love youuuu',
+    btnAccept: 'ok',
+    messLink: './img/cuteCat.jpg' 
+};
