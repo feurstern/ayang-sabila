2281890914abe25c5bb51b431d9f5e158bc9dc10
feuerstern <rio-kurniawan@sakura-schutzstaffel.com>
2022-07-08 19:51:48 +0700

    fixing typo

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     titleWeb: "Ayang Sabila",
-    introTitle: 'Haiiii, malam Sabila. Kangennn sama kamuuu ',
-    introDesc: `Tolong kamu coba explore webnya ya. Mungkin aja ada sesuatu yang menarik wkkwwk. Btw, coba turn on volume-nya atau enggak pake earphone ya.`,
+    introTitle: 'Haiiii, malam Sabila. Udah makan belum? kalo belum makan ya kwkwk',
+    introDesc: `Btw,tolong kamu coba explore webnya ya. Mungkin aja ada sesuatu yang menarik wkkwwk. Btw, coba turn on volume-nya atau enggak pake earphone ya.`,
     btnIntro: 'Next',
     title: 'Kamu sayang sama aku enggak sih?',
     desc: 'Jawab aja yang jujur gpp kok. Oh iya btw, button "enggak" lagi error. Jadinya cuma bisa pake button satu lagi wkkwkwkw',
@@ -10,7 +10,7 @@ const CONFIG = {
     question: 'Ihhhh makasih jawabannya ayang Sabila. Apa ada hal lain yang mau disampaikan?',
     btnReply: 'Next',
     reply: '',
-    mess: 'Makasihhh ayang udah bukan web-nya',
+    mess: 'Makasihhh ayang udah buka web-nya',
     messDesc: 'Aku lakuin begini emang beneran serius kok sama kamu, Sabila . Nanti aku akan tunjukin hal - hal yang lain buat buktiin kalo aku beneran serius sama kamu. Btw, happy weekend .Love youuuu',
     btnAccept: 'ok',
     messLink: './img/cuteCat.jpg' 
